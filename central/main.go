76a9bc3b7e3e72916b6d44cfd57422ff31159944
anibal.plaza <anibal.plazar@usm.cl>
2022-08-27 21:57:58 -0400

    Push1

--- central/main.go
+++ central/main.go
@@ -1,1 +1,37 @@
+package main
 
+import (
+	"context"
+	"fmt"
+	"net"
+
+	pb "github.com/anibalox/distribuidosproyecto/proto"
+
+	"google.golang.org/grpc"
+)
+
+type server struct {
+	pb.UnimplementedCentralServiceServer
+}
+
+func (s *server) Ayuda(ctx context.Context, req *pb.AyudaReq) (*pb.AyudaResp, error) {
+	fmt.Println("creating the wish list " + req.WishList.Name)
+	return &pb.CreateWishListResp{
+		WishListId: req.WishList.Id,
+	}, nil
+}
+
+func main() {
+	listner, err := net.Listen("tcp", ":50051")
+
+	if err != nil {
+		panic("cannot create tcp connection" + err.Error())
+	}
+
+	serv := grpc.NewServer()
+	pb.RegisterWishListServiceServer(serv, &server{})
+	if err = serv.Serve(listner); err != nil {
+		panic("cannot initialize the server" + err.Error())
+	}
+
+}
--- laboratorio/main.go
+++ laboratorio/main.go
@@ -1,1 +1,58 @@
+package main
 
+import (
+	"context"
+	"fmt"
+	"math/rand"
+	"strconv"
+	"time"
+
+	pb "github.com/anibalox/distribuidosproyecto/proto"
+	"google.golang.org/grpc"
+)
+
+type server struct {
+	pb.UnimplementedLaboratorioServiceServer
+}
+
+func generateID() string {
+	rand.Seed(time.Now().Unix())
+	return "ID: " + strconv.Itoa(rand.Int())
+}
+
+func (s *server) PedirSituacion(ctx context.Context, req *pb.SituacionReq) (*pb.SituacionResp, error) {
+	fmt.Println("creating the wish list " + req.WishList.Name)
+	return &pb.CreateWishListResp{
+		WishListId: req.WishList.Id,
+	}, nil
+}
+func (s *server) Finalizar(ctx context.Context, req *pb.FinalizacionReq) (*pb.FinalizacionResp, error) {
+	fmt.Println("creating the wish list " + req.WishList.Name)
+	return &pb.CreateWishListResp{
+		WishListId: req.WishList.Id,
+	}, nil
+}
+
+func main() {
+	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+
+	if err != nil {
+		panic("cannot connect with server " + err.Error())
+	}
+
+	serviceClient := pb.NewWishListServiceClient(conn)
+
+	res, err := serviceClient.Create(context.Background(), &pb.CreateWishListReq{
+		WishList: &pb.WishList{
+			Id:   generateID(),
+			Name: "my wishlist",
+		},
+	})
+
+	if err != nil {
+		panic("wishlist is not created  " + err.Error())
+	}
+
+	fmt.Println(res.WishListId)
+
+}
