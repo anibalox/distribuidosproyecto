9b5c09bfd92e477ebeb2e6673ef1c55de63fd386
felipebuscaglia <felipe.buscaglia@usm.cl>
2022-09-05 14:53:06 -0400

    agregado rabbit central

--- central/main.go
+++ central/main.go
@@ -1,16 +1,23 @@
 package main
 
 import (
+	l "container/list"
 	"fmt"
 	"net"
 	"time"
 
 	pb "github.com/anibalox/distribuidosproyecto/proto"
 	"google.golang.org/grpc"
+
+	//rabbit
+	"log"
+
+	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 type server struct {
 	pb.UnimplementedCentralServiceServer
+	mensaje string
 }
 
 func transformarSituacion(nro int32) string {
@@ -28,11 +35,12 @@ func (s *server) AbrirComunicacion(stream pb.CentralService_AbrirComunicacionSer
 
 	var situacion *pb.SituacionResp
 
-	time.Sleep(5 * time.Second)
 	for situacion, _ = stream.Recv(); situacion.Resuelta == 0; situacion, _ = stream.Recv() {
-		stream.Send(&pb.SituacionReq{Peticion: 1})
+		println("test 1")
 		fmt.Println("Estatus Escuadra " + situacion.NroEscuadra + " : [" + transformarSituacion(situacion.Resuelta) + "]")
 		time.Sleep(5 * time.Second)
+		stream.Send(&pb.SituacionReq{Peticion: 1})
+		println("test 2")
 	}
 	fmt.Println("Estatus Escuadra " + situacion.NroEscuadra + " : [" + transformarSituacion(situacion.Resuelta) + "]")
 	fmt.Println("Retorno a Central Escuadra " + situacion.NroEscuadra + ", Conexion Laboratorio " + situacion.NroLab + " Cerrada")
@@ -44,16 +52,58 @@ func EnviarAyuda(ip string, puerto string) {
 
 }
 
-type mensajes struct {
-	ip     string
-	puerto string
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Panicf("%s: %s", msg, err)
+	}
+}
+func rabbit(serv server) {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"hello", // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	failOnError(err, "Failed to register a consumer")
+
+	for d := range msgs {
+		log.Printf("Received a message: %s", d.Body)
+		serv.mensaje = string(d.Body)
+	}
+
+	//log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+
 }
 
-func main() {
+var queue *l.List
 
-	var lista_mensajes []mensajes // Definir mensajes con Rabbit
-	cantidad_equipos := 2
-	var puerto, ip string
+func main() {
+	var forever chan struct{}
+	queue = l.New()
+	queue.PushBack("hola")
+	queue.PushBack(2)
+	fmt.Println(queue.Front().Value)
 
 	listner, err := net.Listen("tcp", ":50051")
 
@@ -62,18 +112,18 @@ func main() {
 	}
 
 	serv := grpc.NewServer()
-	pb.RegisterCentralServiceServer(serv, &server{})
+	nuevo_server := server{mensaje: "xd"}
+	print(nuevo_server.mensaje + "aqui /n")
+	go rabbit(nuevo_server)
+	pb.RegisterCentralServiceServer(serv, &nuevo_server)
 	if err = serv.Serve(listner); err != nil {
 		panic("cannot initialize the server" + err.Error())
 	}
+	print("test /n")
+	print(nuevo_server.mensaje + "otro /n")
+	<-forever
 
 	//Cambiar ciclo para que se repita hasta senal de termino
-
-	for mensaje := range lista_mensajes {
-		puerto = mensaje.puerto
-		ip = mensaje.ip
-	}
-
 }
 
 //test
